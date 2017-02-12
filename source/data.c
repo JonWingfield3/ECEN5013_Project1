ff99299a3087a044ec6e069108f1ba2c324d6fe1
Jacob Grunwald <jacob.grunwald@colorado.edu>
2017-02-12 13:07:35 -0700

    Fixed pointer manipulation in memory.c and some things in data.c

--- source/data.c
+++ source/data.c
@@ -8,17 +8,17 @@ int8_t* my_itoa(int8_t* str, int32_t data, int32_t base)
 {
 	const int8_t* digits = "0123456789abcdefghijklmnopqrstuvwxyz";
 	uint8_t i = 0;
-	uint32_t u_data;	
+	uint32_t u_data;
 
 	if(!str || base > 36 || base < 2) return NULL;
-	
+
 	if(data < 0 && base == 10)
 	{
 		*(str + i++) = '-';
 		u_data = ((uint32_t)-data);
 		if(data == 0)
-		{// overflow. not very elegant solution but faster than do-while loop	
-			*(str + i++) = '2'; 
+		{// overflow. not very elegant solution but faster than do-while loop
+			*(str + i++) = '2';
 			*(str + i++) = '1';
 			*(str + i++) = '4';
 			*(str + i++) = '7';
@@ -29,24 +29,24 @@ int8_t* my_itoa(int8_t* str, int32_t data, int32_t base)
 			*(str + i++) = '4';
 			*(str + i++) = '8';
 			*(str + i++) = '\0';
-			return str;	
+			return str;
 		}
 	}
-	else u_data = (uint32_t)data; 	
-	
-	do	
+	else u_data = (uint32_t)data;
+
+	do
 	{
 		*(str + i++) = digits[u_data % base];
-		u_data /= base;		
+		u_data /= base;
 	}while(u_data);
-	
+
 	if(str[0] == '-')
 		my_reverse(str + 1, i - 1);
-	else 
+	else
 		my_reverse(str, i);
-	
+
 	*(str + i) = '\0';
-	return str;	
+	return str;
 }
 
 int32_t my_atoi(int8_t* str)
@@ -70,22 +70,20 @@ int8_t big_to_little32(uint32_t* data, uint32_t length)
 {
 	uint32_t i;
 	if(!data) return PTR_ERROR;
-	
-	length >>= 2;	
-	for(i = 0; i < length; ++i)
-		my_reverse((uint8_t*)(data + i), 4);
 
-	return SUCCESS;	
+	for(i = 0; i < length; ++i)
+		my_reverse((((uint8_t*)data) + i), 4);
+
+	return SUCCESS;
 }
 
 int8_t little_to_big32(uint32_t* data, uint32_t length)
 {
 	uint32_t i;
 	if(!data) return PTR_ERROR;
-		
-	length >>= 2;
+
 	for(i = 0; i < length; ++i)
-		my_reverse((uint8_t*)(data + i),  4);
+		my_reverse((((uint8_t*)data) + i),  4);
 
 	return SUCCESS;
 }
@@ -94,8 +92,7 @@ void print_memory(uint8_t* start, uint32_t length)
 {
 	uint32_t i;
 	if(!start) return;
-	
+
 	for(i = 0; i < length; ++i)
 		printf("%x ", *(start + i));
 }
-
